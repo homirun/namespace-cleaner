5aa39f3751089391a907c8a0d0a64152170454a5
homirun <me@homi.run>
2024-12-15 18:03:19 +0900

    applicationリソースも触る

--- cmd/main.go
+++ cmd/main.go
@@ -147,6 +147,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Namespace")
 		os.Exit(1)
 	}
+	if err = (&controller.ApplicationReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Application")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
